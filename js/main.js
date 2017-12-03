4e5f6cdfc184a0fcce02467c719f76f096a8e0fd
Unknown <dionfananie@gmail.com>
2017-12-03 07:24:13 +0700

    update

--- js/main.js
+++ js/main.js
@@ -93,6 +93,36 @@ for (var i = 1; i < 2;  i++) {
         animationDuration: 1500
     }).draw();
 }
+for (var i = 1; i < 2;  i++) {
+    var compass = 'gauge-linear-'+i;
+    console.log(compass)
+    var gaugeCompass = new LinearGauge({
+        renderTo: compass,
+        width:120,
+        height:220,
+        units:"°C",
+        minValue:0,
+        maxValue:220,
+        majorTicks:[0,20,40,60,80,100,120,140,160,180,200,220],
+        minorTicks:2,
+        strokeTicks:true,
+        highlights:[ {from: 100, to: 220, color: "rgba(200, 50, 50, .75)"} ],
+        colorPlate:"#fff",
+        borderShadowWidth:0,
+        borders:false,
+        needleType:"arrow",
+        needleWidth:2,
+        animationDuration:1500,
+        animationRule:"linear",
+        tickSide:"left",
+        numberSide:"left",
+        needleSide:"left",
+        barStrokeWidth:1,
+        barWidth:55,
+        barBeginCircle:false,
+        value:75,
+    }).draw();
+}
 
 Highcharts.chart('tekanan_udara', {
             chart: {
